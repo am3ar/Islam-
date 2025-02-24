a4bd3669d347ad334381185600b6a16864821b97
xz_3x <ammaresa19@gmail.com>
2025-02-24 22:58:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,3 @@
 document.getElementById('theme-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
-
-// التأثير الحركي على النصوص المتحركة
-setInterval(function() {
-    const texts = [
-        "إِنَّ هَذَا الْقُرْآنَ يَهْدِي لِلَّتِي هِيَ أَقْوَمُ",
-        "إِنَّ مَعَ الْعُسْ
