5ee7e790fddc0a6acd7b9cf7f4806168d3e302ab
xz_3x <ammaresa19@gmail.com>
2025-02-25 13:33:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const verses = document.querySelectorAll('.verse');
+    const hadiths = document.querySelectorAll('.hadith');
+
     verses.forEach(verse => {
         verse.addEventListener('click', () => {
             verse.classList.toggle('active');
         });
     });
-}); 
+
+    hadiths.forEach(hadith => {
+        hadith.addEventListener('click', () => {
+            hadith.classList.toggle('active');
+        });
+    });
+});
