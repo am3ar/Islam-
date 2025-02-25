ac521f300412a5f079a4fb9d3dc21e25a36d5ed7
xz_3x <ammaresa19@gmail.com>
2025-02-25 12:34:05 +0300

    Update script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// مثال على وظيفة تفاعلية: تفاعل مع التمرير
-window.addEventListener("scroll", function() {
-    let header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.style.backgroundColor = "#111";
-    } else {
-        header.style.backgroundColor = "#333";
-    }
-});
